8f94cd9c21073f9e48f5663c04af25409072097a
Marcelo Damasceno <marcelo.damasceno@globality.com.br>
2025-09-15 14:51:34 -0300

    feat: implement login user component with authentication logic

---
+++ nda-manager/src/app/components/login-user/login-user.component.ts
@@ -0,0 +1,40 @@
+import { Component, signal } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { ClientService } from '../../services/client.service';
+
+@Component({
+  selector: 'app-login-user',
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterModule],
+  templateUrl: './login-user.component.html',
+  styleUrl: './login-user.component.scss'
+})
+export class LoginUserComponent {
+  email = signal('');
+  password = signal('');
+  message = signal('');
+  loggedIn = signal(false);
+
+  constructor(private clientService: ClientService, private router: Router) {}
+  goToRegister() {
+    this.router.navigate(['/register']);
+  }
+
+  login() {
+    if (!this.email() || !this.password()) {
+      this.message.set('Please enter email and password.');
+      return;
+    }
+    const success = this.clientService.authenticateClient(this.email(), this.password());
+    if (success) {
+      this.loggedIn.set(true);
+      this.message.set('Login successful!');
+    } else {
+      this.message.set('Invalid credentials.');
+      this.loggedIn.set(false);
+    }
+  }
+}
