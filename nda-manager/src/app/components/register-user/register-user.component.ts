3c64f4bb6112f914175d10b922bf21892b931318
Marcelo Damasceno <marcelo.damasceno@globality.com.br>
2025-09-15 14:56:23 -0300

    feat: add register user component

---
+++ nda-manager/src/app/components/register-user/register-user.component.ts
@@ -0,0 +1,41 @@
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ClientService } from '../../services/client.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-register-user',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './register-user.component.html',
+  styleUrl: './register-user.component.scss'
+})
+export class RegisterUserComponent {
+  name = signal('');
+  email = signal('');
+  password = signal('');
+  message = signal('');
+
+  constructor(private clientService: ClientService, private router: Router) {}
+
+  register() {
+    if (!this.name() || !this.email() || !this.password()) {
+      this.message.set('Please fill all fields.');
+      return;
+    }
+    const client = this.clientService.registerClient(
+      this.name(),
+      this.email(),
+      this.password()
+    );
+    this.message.set(`User registered: ${client.name}`);
+    this.name.set('');
+    this.email.set('');
+    this.password.set('');
+    setTimeout(() => {
+      this.router.navigate(['/login']);
+    }, 1200);
+  }
+
+}
