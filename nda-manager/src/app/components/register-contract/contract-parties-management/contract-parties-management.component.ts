1e426311f9d63313af17b5275bfc23e9e1b77076
Marcelo Damasceno <marcelo.damasceno@globality.com.br>
2025-11-25 14:40:10 -0300

    feat: Implement party management table and modal for adding/editing parties

--- nda-manager/src/app/components/register-contract/contract-parties-management/contract-parties-management.component.ts
+++ nda-manager/src/app/components/register-contract/contract-parties-management/contract-parties-management.component.ts
@@ -1,11 +1,11 @@
-import { Component, input, output } from '@angular/core';
+import { Component, input, output, signal } from '@angular/core';
 import { ContactInfo } from '../../../models/contract.model';
-import { ContactPersonFormComponent } from '../contact-person-form/contact-person-form.component';
+import { PartyEditModalComponent } from '../party-edit-modal/party-edit-modal.component';
 
 @Component({
   selector: 'app-contract-parties-management',
   standalone: true,
-  imports: [ContactPersonFormComponent],
+  imports: [PartyEditModalComponent],
   templateUrl: './contract-parties-management.component.html',
   styleUrl: './contract-parties-management.component.scss'
 })
@@ -16,6 +16,11 @@ export class ContractPartiesManagementComponent {
   singularTitle = input.required<string>();
 
   partiesChange = output<ContactInfo[]>();
+
+  // Modal state
+  isModalOpen = signal<boolean>(false);
+  editingPartyIndex = signal<number>(-1);
+  editingParty = signal<ContactInfo | undefined>(undefined);
 
   addParty() {
     const emptyContact: ContactInfo = {
@@ -36,7 +41,15 @@ export class ContractPartiesManagementComponent {
       officialEmail: ''
     };
     
-    this.partiesChange.emit([...this.parties(), emptyContact]);
+    this.editingParty.set(emptyContact);
+    this.editingPartyIndex.set(-1); // -1 indicates new party
+    this.isModalOpen.set(true);
+  }
+
+  editParty(index: number) {
+    this.editingParty.set({ ...this.parties()[index] });
+    this.editingPartyIndex.set(index);
+    this.isModalOpen.set(true);
   }
 
   removeParty(index: number) {
@@ -44,9 +57,39 @@ export class ContractPartiesManagementComponent {
     this.partiesChange.emit(updated);
   }
 
-  updateParty(index: number, contact: ContactInfo) {
+  onModalClose() {
+    this.isModalOpen.set(false);
+    this.editingParty.set(undefined);
+    this.editingPartyIndex.set(-1);
+  }
+
+  onModalSave(contact: ContactInfo) {
     const updated = [...this.parties()];
-    updated[index] = contact;
+    const index = this.editingPartyIndex();
+    
+    if (index === -1) {
+      // Adding new party
+      updated.push(contact);
+    } else {
+      // Updating existing party
+      updated[index] = contact;
+    }
+    
     this.partiesChange.emit(updated);
+    this.onModalClose();
+  }
+
+  getIdentificationDisplay(contact: ContactInfo): string {
+    const typeMap = {
+      'cpf': 'CPF',
+      'cnpj': 'CNPJ',
+      'passport': 'Passport',
+      'other': 'Other'
+    };
+    return `${typeMap[contact.identification.type]}: ${contact.identification.number}`;
+  }
+
+  getEntityTypeDisplay(entityType: 'individual' | 'company'): string {
+    return entityType === 'individual' ? 'Individual' : 'Company';
   }
 }
---
+++ nda-manager/src/app/components/register-contract/party-edit-modal/party-edit-modal.component.ts
@@ -0,0 +1,109 @@
+import { Component, input, output, signal, OnInit, OnChanges } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ContactInfo } from '../../../models/contract.model';
+
+@Component({
+  selector: 'app-party-edit-modal',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './party-edit-modal.component.html',
+  styleUrl: './party-edit-modal.component.scss'
+})
+export class PartyEditModalComponent implements OnInit, OnChanges {
+  isOpen = input<boolean>(false);
+  contact = input<ContactInfo>();
+  loading = input<boolean>(false);
+  title = input<string>('Edit Party');
+
+  close = output<void>();
+  save = output<ContactInfo>();
+
+  private editingContact = signal<ContactInfo>({
+    name: '',
+    entityType: 'individual',
+    companyName: '',
+    address: {
+      street: '',
+      city: '',
+      state: '',
+      postalCode: '',
+      country: ''
+    },
+    identification: {
+      type: 'cpf',
+      number: ''
+    },
+    officialEmail: ''
+  });
+
+  ngOnInit() {
+    if (this.contact()) {
+      this.editingContact.set({ ...this.contact()! });
+    }
+  }
+
+  ngOnChanges() {
+    if (this.contact()) {
+      this.editingContact.set({ ...this.contact()! });
+    }
+  }
+
+  onClose() {
+    this.close.emit();
+  }
+
+  onSave() {
+    if (this.isValidForm()) {
+      this.save.emit(this.editingContact());
+      this.onClose();
+    }
+  }
+
+  onFieldChange(field: keyof ContactInfo, value: any) {
+    this.editingContact.update(contact => ({
+      ...contact,
+      [field]: value
+    }));
+  }
+
+  onAddressChange(field: keyof ContactInfo['address'], value: string) {
+    this.editingContact.update(contact => ({
+      ...contact,
+      address: {
+        ...contact.address,
+        [field]: value
+      }
+    }));
+  }
+
+  onIdentificationChange(field: keyof ContactInfo['identification'], value: string) {
+    this.editingContact.update(contact => ({
+      ...contact,
+      identification: {
+        ...contact.identification,
+        [field]: value
+      }
+    }));
+  }
+
+  private isValidForm(): boolean {
+    const contact = this.editingContact();
+    return !!(
+      contact.name &&
+      contact.entityType &&
+      (contact.entityType === 'individual' || contact.companyName) &&
+      contact.address.street &&
+      contact.address.city &&
+      contact.address.state &&
+      contact.address.postalCode &&
+      contact.address.country &&
+      contact.identification.type &&
+      contact.identification.number &&
+      contact.officialEmail
+    );
+  }
+
+  get editContact() {
+    return this.editingContact();
+  }
+}
